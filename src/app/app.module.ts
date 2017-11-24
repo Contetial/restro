635e755b170659a32f60ee4d568a2fab091d0783
gandharpatwardhan <patwardhan.gandhar@gmail.com>
2017-11-24 12:40:58 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppRoutes } from "./app.routes";
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { UserprofileComponent } from './views/userprofile/userprofile.component';
+import { WindowService } from './service/window.service';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCRCdZGuk2QuQ2nArHASO3_BKvVzYvl60w",
+    authDomain: "restro-e78da.firebaseapp.com",
+    databaseURL: "https://restro-e78da.firebaseio.com",
+    projectId: "restro-e78da",
+    storageBucket: "restro-e78da.appspot.com",
+    messagingSenderId: "979815721457"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    UserprofileComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     AppRoutes
   ],
-  providers: [],
+  providers: [AngularFireAuth, AngularFireDatabase, WindowService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+import { WindowService } from '../../service/window.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
+
+export class PhoneNumber {
+  country: string;
+  
+  area: string;
+  prefix: string;
+  line: string;
+  // format phone numbers as E.164
+  get e164() {
+    const num = "+91" + this.line
+    //return `+${num}`
+    return num;
+  }
+};
 
 @Component({
   selector: 'app-login',
@@ -8,9 +28,74 @@ import { Router } from "@angular/router";
 })
 export class LoginComponent implements OnInit {
 
+//user: Observable<firebase.User>;
+windowRef: any;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+  phoneNumber = new PhoneNumber();
+  verificationCode: string;
+  user: any;
   
-  constructor(private router: Router) { }
+  constructor(private router: Router, public afAuth: AngularFireAuth, public af: AngularFireDatabase, private win: WindowService) {
+  this.items = af.list('/messages', {
+      query: {
+        limitToLast: 50
+      }
+    });
+
+    this.user = this.afAuth.authState;
+  };
   
+  ngOnInit() {
+    this.windowRef = this.win.windowRef;
+    this.windowRef.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('phone-sign-in-recaptcha', {
+  'size': 'invisible',
+});
+    this.windowRef.recaptchaVerifier.render();
+  };
+  
+  sendLoginCode() {
+  
+  console.log("here1");
+    
+
+    const num = this.phoneNumber.e164;
+    firebase.auth().signInWithPhoneNumber(num, this.windowRef.recaptchaVerifier)
+            .then(result => {
+                this.windowRef.confirmationResult = result;
+                
+            })
+            .catch( error => console.log(error) );
+   // const appVerifier = this.windowRef.recaptchaVerifier;
+    
+  }
+  verifyLoginCode() {
+    this.windowRef.confirmationResult
+                  .confirm(this.verificationCode)
+                  .then( result => {
+                    this.user = result.user;
+                    console.log(this.user);
+                    this.router.navigate(['/dashboard']);
+    })
+    .catch( error => console.log(error, "Incorrect code entered?"));
+  }
+  
+  login() {
+  alert("here");
+    this.afAuth.auth.signInAnonymously();
+    console.log(this.items);
+};
+
+logout() {
+    this.afAuth.auth.signOut();
+}
+
+Send(desc: string) {
+    this.items.push({ message: desc});
+    console.log(this.items);
+    this.msgVal = '';
+}
+  /*
   login(myemail: string, mypass: string){
   if(myemail == "test" && mypass=="test")
   {
@@ -20,9 +105,8 @@ export class LoginComponent implements OnInit {
   {
     alert("Invalid email / pwd");
   }
-  }
-
-  ngOnInit() {
-  }
+  }*/
+  
+ 
 
 }
